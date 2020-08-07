8f7dc37e81197b7b8ab6cd9a5e64bc2af1f4f2ff
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-07 22:28:05 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,8 @@ class SessionsController < ApplicationController
   end
 
 #login action
+#redirects to login if name is nil or empty
+#creates session is name is given
   def create
     if params[:name].present?
       session[:name] = params[:name]
@@ -12,7 +14,8 @@ class SessionsController < ApplicationController
     end
   end
 
-#logout action 
+#logout action
+# clears session if name was set 
   def destroy
     if session[:name].present?
       session.delete :name
