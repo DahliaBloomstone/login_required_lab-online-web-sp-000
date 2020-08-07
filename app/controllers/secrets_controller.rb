3d78389d5e0dc33261e4c3fb442f6724e37bdd98
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-08-07 22:27:34 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,8 +1,10 @@
 class SecretsController < ApplicationController
   before_action :require_login
 
+#get method redirect to login if not logged in
+#show secret if logged in 
   def show
-    #return guard: unless session includes :user_id we return an error 
+    #return guard: unless session includes :user_id we return an error
     #return head(:forbidden) unless session.include? :user_id
    # @document = Document.find(params[:id])
   end
